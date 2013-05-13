7d1e6eada9ce87c40ea1ac45fa5f289c49eb7884
Christopher Thielen <cthielen@gmail.com>
2013-05-13 12:44:18 -0700

    Initial commit. Default Rails 3.2 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DssDw::Application.config.secret_token = '400bb5ed503aedaefd7b70a525b0cd0c506c34feb91297bba7d5cfbe8c11707d3cc407346e15fc9f10611dacba75cd54573abac7524eaeaae981363ff24c051b'
