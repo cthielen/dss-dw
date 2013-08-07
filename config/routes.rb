ac053ec4875d7b06a5003cb5c52c02ad311d6a94
Christopher Thielen <cthielen@gmail.com>
2013-08-06 19:46:30 -0700

    Implemented basic API versioning support. See http://railscasts.com/episodes/350-rest-api-versioning.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+require 'api_constraints'
+
 DssDw::Application.routes.draw do
-  resources :courses
+  namespace :api, defaults: {format: 'json'} do
+    scope module: :v0, constraints: ApiConstraints.new(version: 0, default: true) do
+      resources :courses
+    end
+    # scope module: :v2, constraints: ApiConstraints.new(version: 2, default: true) do
+    #   resources :products
+    # end
+  end
 end
