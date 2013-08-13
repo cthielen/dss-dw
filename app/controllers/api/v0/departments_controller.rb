98acadeb531da2d6543742d4fac85bad1c6f8710
Christopher Thielen <cthielen@gmail.com>
2013-08-13 14:18:04 -0700

    Implemented department show view.

--- app/controllers/api/v0/departments_controller.rb
+++ app/controllers/api/v0/departments_controller.rb
@@ -12,10 +12,25 @@ module Api
         render "api/v0/departments/index"
       end
       
+      def show
+        if @department
+          logger.info "#{current_user.log_identifier}@#{request.remote_ip}: Loaded department view (show) for #{@department.code}."
+          render "api/v0/departments/show"
+        else
+          logger.info "#{current_user.log_identifier}@#{request.remote_ip}: Attempted to load department view (show) for invalid code or ID #{params[:id]}."
+          render :text => "Invalid department code or ID '#{params[:id]}'.", :status => 404
+        end
+      end
+      
       private
       
       def load_departments
         @departments = Department.all
+      end
+      
+      def load_department
+        @department = Department.find_by_id(params[:id])
+        @department = Department.find_by_code(params[:id]) unless @department
       end
     end
   end
