a9c6b940817737294e94be48b9d72ad1d5f7eeef
Christopher Thielen <cthielen@gmail.com>
2013-08-15 09:52:47 -0700

    Require section active status to be true or false, not merely present

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,7 +1,8 @@
 class Section < ActiveRecord::Base
   attr_accessible :active, :actual_enrollment, :campus_id, :max_enrollment, :sequence
 
-  validates_presence_of :campus_id, :sequence, :actual_enrollment, :max_enrollment, :active
+  validates_presence_of :campus_id, :sequence, :actual_enrollment, :max_enrollment
+  validates_inclusion_of :active, :in => [true, false]
   
   belongs_to :campus
   belongs_to :course_offering
