d8be3c0404e5402892d9c794426445c6519b3087
Christopher Thielen <cthielen@gmail.com>
2013-09-24 19:06:13 -0700

    Fixture update

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -4,7 +4,7 @@ require 'rails/performance_test_help'
 class BrowsingTest < ActionDispatch::PerformanceTest
   # Refer to the documentation for all available options
   self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory] }
-
+  
   def test_courses_index
     get '/api/courses'
   end
