e4d9db1a332f8a3f486d839b731c82c2bef0468d
Christopher Thielen <cthielen@gmail.com>
2013-09-26 10:01:26 -0700

    Run IAM import every 4 hours

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,12 +5,13 @@ job_type :envcommand, 'cd :path && RAILS_ENV=:environment :task'
 every 8.hours do
   #command "/usr/bin/some_great_command"
   #runner "MyModel.some_method"
-  #rake "some:great:rake:task"
-  envcommand 'bundle exec rake banner:import'
+  rake "banner:import"
+  #envcommand 'bundle exec rake banner:import'
 end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+
+every 4.hours do
+  rake "iam:import"
+end
 
 # Learn more: http://github.com/javan/whenever
+# pid, size = `ps ax -o pid,rss | grep -E "^[[:space:]]*#{$$}"`.strip.split.map(&:to_i)
