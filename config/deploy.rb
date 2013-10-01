0f61347fe99aa8d4a2bfbc9c69653b14a7324e5d
Christopher Thielen <cthielen@gmail.com>
2013-10-01 08:43:12 -0700

    Don't forget to symlink IAM config

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,6 +59,7 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/banner.yml #{release_path}/config/banner.yml"
+    run "ln -nfs #{shared_path}/config/iam.yml #{release_path}/config/iam.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
